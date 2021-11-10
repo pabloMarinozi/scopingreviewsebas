92d34ced7b09f544561130ff5ab98e77feec9a7a
pabloMarinozi <marinozipablo+github@gmail.com>
2021-11-10 18:37:55 -0300

    Agrega extracción de datos, carga bibliográfica y descarga de citaciones

--- src/clases.py
+++ src/clases.py
@@ -6,11 +6,10 @@ Created on Mon Nov  9 13:03:16 2020
 @author: pablo
 """
 
-
 from mongoengine import Document,EmbeddedDocument
 from mongoengine import StringField,IntField,ListField,BooleanField,URLField,ReferenceField,EmbeddedDocumentField
 
-
+#Clases creadas para mongodb
 class Visual_Feature(Document):
     name = StringField(required=True, primary_key=True)
     type = StringField(required=True)  
@@ -31,23 +30,30 @@ class Visual_Task(EmbeddedDocument):
     viticultural_variable = ListField(StringField(max_length=50))
     viticultural_variable_details = StringField()
     monitoring = BooleanField(required=True)
-    
+    variety = ListField(StringField())
+    driving_systems = ListField(StringField())
     
 class Institution(Document):
-    name = StringField(required=True, primary_key=True)
+    name = StringField(required=True, primary_key=True) #affiliation[]
     #country = StringField(required=True)
 
 class Finantial_Institution(Document):
+    doi = StringField()   #['funder']['DOI']
     name = StringField(required=True, primary_key=True)
-    country = StringField(required=True)
+    #country = StringField()
 
 class Author(Document):
-    name = StringField(required=True, primary_key=True)
-    scopusID = StringField()
+    orcid = StringField()
+    authenticated_orcid = BooleanField()
+    name = StringField(required=True, primary_key=True) #concatenar
+    familyName = StringField()
+    firstName = StringField()
+    #scopusID =StringField() #hay q sacarlo
 
 class Author_Affiliation(EmbeddedDocument):
     institution = ReferenceField(Institution)
     author = ReferenceField(Author)
+    sequence = StringField(max_length=15)  #first or additional 
     
 # class Inclusion(EmbeddedDocument):
 #     inclusion = BooleanField(required=True)
@@ -55,7 +61,7 @@ class Author_Affiliation(EmbeddedDocument):
 #     criteria = ListField(StringField())
         
 class Paper(Document):
-    title = StringField(required=True)
+    title = StringField(required=True)    
     abstract = StringField(required=True)
     doi = StringField(required=True, primary_key=True)
     on_revision = StringField()
@@ -76,5 +82,11 @@ class Paper(Document):
     viticultural_aspects = StringField(max_length=50)
     research_goal = ListField(StringField())
     practical_contibution = StringField()
-    
-
+    isOnlyReference = BooleanField()
+    citationsSearched = BooleanField()
+    citedBy = ListField(StringField())
+    #isLoadedBefore = BooleanField()
+    bibliographyIsLoaded = BooleanField()
+    references = ListField(StringField())
+    #research_goal = ListField(StringField()) VARIETALES
+    #research_goal = ListField(StringField()) SISTEMAS DE CONDUCCION
